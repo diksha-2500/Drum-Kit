624a409203f7ec730f56d3c4d4f13f6fb44f972d
diksha-2500 <63575450+diksha-2500@users.noreply.github.com>
2020-12-14 14:05:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+for (var i = 0; i < 7; i++) {
+      document.querySelectorAll("button")[i].addEventListener("click", function () {
+            //alert("pressed");
+            var buttonInnerHtml=this.innerHTML;
+            makeSound(buttonInnerHtml);
+            buttonAnimation(buttonInnerHtml);
+           
+      });
+}
+document.addEventListener("keypress",function(event){
+      console.log(event);
+     makeSound(event.key);
+     buttonAnimation(event.key);
+})
+
+function makeSound(key){
+      switch (key) {
+            case "w":
+                  var aud=new Audio('/sounds/tom-1.mp3');
+                  aud.play();
+                  break;
+            case "a":
+                  var aud=new Audio('/sounds/tom-2.mp3');
+                  aud.play();
+                  break;
+            case "s":
+                  var aud=new Audio('/sounds/tom-3.mp3');
+                  aud.play();
+                  break; 
+            case "d":
+                  var aud=new Audio('/sounds/tom-4.mp3');
+                  aud.play();
+                  break;    
+            case "j":
+                  var aud=new Audio('/sounds/crash.mp3');
+                  aud.play();
+                  break;  
+            case "k":
+                  var aud=new Audio('/sounds/kick-bass.mp3');
+                  aud.play();
+                  break; 
+            case "l":
+                  var aud=new Audio('/sounds/snare.mp3');
+                  aud.play();
+                  break;            
+            default:
+                  console.log(buttonInnerHtml);
+                  break;
+      }
+}
+function buttonAnimation(key){
+      var currentButton=document.querySelector("."+key);
+      currentButton.classList.add("pressed");
+      setTimeout(function(){
+            currentButton.classList.remove("pressed");
+      },100);
+}
+
